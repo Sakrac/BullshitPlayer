898297c061988475320480cb25b5ec82cbe4b1e6
Carl-Henrik Skårstedt <cskarstedt@gmail.com>
2021-01-05 13:56:03 +0100

    using the included version of x65 to build sample project (doh!)

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
-..\x65\bin\x64\x65 test.s test.prg -lst=testcode.lst -sym test.sym -vice test.vs 
+x65\x65 test.s test.prg -lst=testcode.lst -sym test.sym -vice test.vs 
 \vice31\x64sc -moncommands test.vs test.prg
